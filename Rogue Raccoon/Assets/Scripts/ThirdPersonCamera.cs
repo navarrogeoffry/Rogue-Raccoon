96de3bd9352a88309f5f9bc7a1c8467a962cde60
Geoffry Navarro <navarrogeoffry@gmail.com>
2024-04-25 19:25:19 -0700

    Character Controller Backup

---
+++ Rogue Raccoon/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObject;
+    public Rigidbody rb;
+
+    public float rotationSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate Camera Orientation
+        Vector3 viewDirection = player.position - new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        orientation.forward = viewDirection.normalized;
+
+        //Rotate Player Object
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 inputDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if (inputDirection != Vector3.zero)
+            player.forward = Vector3.Lerp(playerObject.forward, inputDirection.normalized, Time.deltaTime * rotationSpeed);
+    }
+}
